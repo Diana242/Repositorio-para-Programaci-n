04f270eb770005dc4865061b45c367b743d907c6
Diana242 <marceji08@gmail.com>
2022-10-13 16:40:48 -0600

    Práctica Cuatro

---
+++ Practica4/NombreClase.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practica4;
+
+/**
+ *
+ * @author marce    
+ */
+public class NombreClase {
+    
+    public String nombre;
+    public String apellidos;
+    public int edad;
+    public String dirección;
+    public int grado;
+    
+    public static void prueba (){
+        System.out.print("Practica4Folder.EstudiantesClase.prueba()");
+    
+    }
+    
+    public static void agregarDatos (){
+        
+    }
+    
+    public static void mostrarDatos (){
+        
+    }
+    
+    public static void confirmarDatos (){
+        
+    }
+    
+    public static void modificarDatos (){
+        
+    }
+    
+    public static void asignarGrupo (){
+        
+    }
+    
+    public static void asignarSede (){
+        
+    }
+    
+    public static void asignarAula (){
+        
+    }
+    
+    public static void transferirDatos (){
+        
+    }
+    
+    public static void eliminarDatos (){
+        
+    }
+    
+    }
